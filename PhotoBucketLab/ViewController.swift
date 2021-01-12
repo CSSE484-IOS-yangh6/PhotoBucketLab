ae9b58be67b00ad5950f2306fe2f1e4db6cb690c
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-01-12 16:11:54 +0800

    Initial Commit

---
+++ PhotoBucketLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoBucketLab
+//
+//  Created by Hanyu Yang on 2021/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
